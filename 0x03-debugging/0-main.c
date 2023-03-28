ab27834450c9bd1cfd8bb79216e1b6fad4857249
ThapiiM <mmathapelot.masemola@gmail.com>
2023-03-28 15:36:21 -0700

    0x03-Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests for a positive or negative function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or negative(i);
+
+	return (0);
+}
